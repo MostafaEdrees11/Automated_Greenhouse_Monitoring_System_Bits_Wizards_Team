832c7304f84de89f3c5a4f7a0158fbb5948d1fe1
samar ibrahim <samaribrahim@std.mans.edu.eg>
2024-07-06 23:48:52 +0300

    add LDR

---
+++ Automated_Greenhouse_Monitoring_System/HAL/LDR/LDR_Priv.h
@@ -0,0 +1,13 @@
+/*
+ * LDR_Priv.h
+ *
+ *  Created on: Jul 4, 2024
+ *      Author: user
+ */
+
+#ifndef HAL_LDR_LDR_PRIV_H_
+#define HAL_LDR_LDR_PRIV_H_
+
+#define NOTCONNECTED 0xFF
+
+#endif /* HAL_LDR_LDR_PRIV_H_ */
