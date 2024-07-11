3a23da72d671976b757195ebd361f2702900ca95
Fathy Anas <fathyanas10717@gmail.com>
2024-07-11 04:47:17 +0300

    Added the display files to display sensors readings and system state

---
+++ Automated_Greenhouse_Monitoring_System_Microchip/Automated_Greenhouse_Monitoring_System_Microchip/APP/Display/Display_config.h
@@ -0,0 +1,13 @@
+/*
+ * Display_config.h
+ *
+ *  Created on: Jul 8, 2024
+ *      Author: Fathy Anas
+ */
+
+#ifndef APP_DISPLAY_DISPLAY_CONFIG_H_
+#define APP_DISPLAY_DISPLAY_CONFIG_H_
+
+
+
+#endif /* APP_DISPLAY_DISPLAY_CONFIG_H_ */
